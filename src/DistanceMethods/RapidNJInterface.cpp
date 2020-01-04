e4bcce7040d60785d070dd7fcc354643880ab80e
Pranjal Vachaspati <pr@nj.al>
2020-01-04 11:46:23 -0500

    fix rapidnjinterface on windows

--- src/DistanceMethods/RapidNJInterface.cpp
+++ src/DistanceMethods/RapidNJInterface.cpp
@@ -3,7 +3,8 @@
 #include "glog/logging.h"
 #include <iostream>
 
-string RapidNJ (TaxonSet& ts, DistanceMatrix& dm) {
+std::string RapidNJ(TaxonSet &ts, DistanceMatrix &dm) {
+
   LOG(ERROR) << "RapidNJ is not supported on Windows" << std::endl;
   return "RapidNJ is not supported on Windows";
 }
@@ -13,44 +14,37 @@ string RapidNJ (TaxonSet& ts, DistanceMatrix& dm) {
 #include "third_party/rapidNJ/rapidNJ.h"
 #include <sstream>
 
-string RapidNJ (TaxonSet& ts, DistanceMatrix& dm)
-{
+std::string RapidNJ(TaxonSet &ts, DistanceMatrix &dm) {
   bool dmreader_verbose = true;
   int matrixSize = ts.size();
   bool halfMatrix = false;
-  vector<string> sequenceNames;
-  distType** matrix;
+  std::vector<std::string> sequenceNames;
+  distType **matrix;
 
   for (Taxon t : ts) {
     sequenceNames.push_back(ts[t]);
   }
 
-  matrix = new distType*[matrixSize];
+  matrix = new distType *[matrixSize];
   for (int t = 0; t < matrixSize; t++) {
     matrix[t] = new distType[matrixSize];
-    for (int s = 0; s < matrixSize; s++) {      
-      matrix[t][s] = dm(t,s);
+    for (int s = 0; s < matrixSize; s++) {
+      matrix[t][s] = dm(t, s);
     }
   }
 
-
-
-  
-  
-  distMatrixReader* reader = new distMatrixReader(dmreader_verbose, matrixSize, halfMatrix, &sequenceNames, matrix);
-
+  distMatrixReader *reader = new distMatrixReader(
+      dmreader_verbose, matrixSize, halfMatrix, &sequenceNames, matrix);
 
   ProgressBar pb;
   rapidNJ rnj(reader, matrixSize, false, &pb);
-  polytree* tree = rnj.run();
-  
+  polytree *tree = rnj.run();
 
   std::stringstream out;
 
   tree->serialize_tree(out);
 
   return out.str();
-  
 }
 
 #endif
